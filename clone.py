dcbe4dfa0c5b571c736d1af094081e5f77c2e22a
manujamnju-art <manujamnju@gmail.com>
2025-09-29 15:03:50 +0530

    Update clone.py

--- clone.py
+++ clone.py
@@ -1,6 +1,10 @@
 a = int(input("Enter the first number (a): "))
 b = int(input("Enter the second number (b): "))
 
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
 a, b = b, a
 
 print("After swapping:")
